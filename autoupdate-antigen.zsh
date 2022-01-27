819b3fa39ddb054b37451e352bf6e3e16bf80b65
Jacob Birkett <jacob@birkett.dev>
2022-01-27 08:46:51 -0700

    Add check for out-of-date plugin source file

--- autoupdate-antigen.zsh
+++ autoupdate-antigen.zsh
@@ -20,6 +20,18 @@ if [ -z "$ANTIBODY_SYSTEM_UPDATE_DAYS" ]; then
   ANTIBODY_SYSTEM_UPDATE_DAYS=7
 fi
 
+if [ -z "$ANTIBODY_PLUGIN_LIST_F" ]; then
+  if [ -f '~/.zsh_plugins.txt' ]; then
+    ANTIBODY_PLUGIN_LIST_F='~/.zsh_plugins.txt'
+  fi
+fi
+
+if [ -z "$ANTIBODY_PLUGIN_SOURCE_F" ]; then
+  if [ -f '~/.zsh_plugins.sh' ]; then
+    ANTIBODY_PLUGIN_SOURCE_F='~/.zsh_plugins.sh'
+  fi
+fi
+
 if [ -z "$ANTIBODY_SYSTEM_RECEIPT_F" ]; then
   ANTIBODY_SYSTEM_RECEIPT_F='.antibody_system_lastupdate'
 fi
@@ -46,6 +58,14 @@ plugins_seconds=$(expr ${day_seconds} \* ${ANTIBODY_PLUGIN_UPDATE_DAYS})
 last_plugin=$(check_interval ${ANTIBODY_PLUGIN_RECEIPT_F})
 last_system=$(check_interval ${ANTIBODY_SYSTEM_RECEIPT_F})
 
+if [ ! -z "$ANTIBODY_PLUGIN_LIST_F" ]; then
+  last_change=$(date -f "$ANTIBODY_PLUGIN_LIST_F" +%s)
+fi
+
+if [ ! -z "$ANTIBODY_PLUGIN_SOURCE_F" ]; then
+  last_bundle=$(date -f "$ANTIBODY_PLUGIN_SOURCE_F" +%s)
+fi
+
 if [ ${last_plugin} -gt ${plugins_seconds} ]; then
   if [ ! -z "$ANTIBODY_AUTOUPDATE_VERBOSE" ]; then
     echo "It has been $(expr ${last_plugin} / $day_seconds) days since your antibody plugins were updated"
@@ -64,11 +84,23 @@ if [ ${last_system} -gt ${system_seconds} ]; then
   $(date +%s > ~/${ANTIBODY_SYSTEM_RECEIPT_F})
 fi
 
+if [ ! -z "$last_change" ]; then
+  if [ -z "$last_bundle" ] || [ ${last_change} -gt ${last_bundle} ]; then
+    if [ ! -z "$ANTIBODY_AUTOUPDATE_VERBOSE" ]; then
+      echo "Changes were made to your plugin list, but have not been bundled yet"
+      echo "Updating antibody plugins..."
+    fi
+    antibody bundle < "$ANTIBODY_PLUGIN_LIST_F" > "$ANTIBODY_PLUGIN_SOURCE_F"
+  fi
+fi
+
 # clean up after ourselves
 unset ANTIBODY_PLUGIN_RECEIPT_F
 unset ANTIBODY_PLUGIN_UPDATE_DAYS
 unset ANTIBODY_SYSTEM_RECEIPT_F
 unset ANTIBODY_SYSTEM_UPDATE_DAYS
+unset ANTIBODY_PLUGIN_LIST_F
+unset ANTIBODY_PLUGIN_SOURCE_F
 unset day_seconds
 unset last_plugin
 unset last_system
