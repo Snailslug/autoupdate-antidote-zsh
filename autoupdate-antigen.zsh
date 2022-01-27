4850953016f8246b84f9adf1b7d3c081a074cf0c
Jacob Birkett <jacob@birkett.dev>
2022-01-27 09:01:18 -0700

    Update copyright

--- autoupdate-antigen.zsh
+++ autoupdate-antigen.zsh
@@ -1,11 +1,12 @@
+# Copyright 2014 Jacob Birkett <jacob@birkett.dev>
 # Copyright 2014 Joe Block <jpb@unixorn.net>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -87,9 +88,7 @@ unset ANTIBODY_PLUGIN_UPDATE_DAYS
 unset ANTIBODY_PLUGIN_LIST_F
 unset ANTIBODY_PLUGIN_SOURCE_F
 unset day_seconds
-unset last_plugin
-unset last_system
 unset plugins_seconds
-unset system_seconds
+unset last_plugin
 
 unset -f check_interval
